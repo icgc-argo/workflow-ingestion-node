213930b37d0aa74ed18f8f40c317405833ee0e1d
jaserud <udjaser@gmail.com>
2020-10-01 12:50:42 -0400

    remove comment

--- src/main/java/org/icgc_argo/workflowingestionnode/WorkflowIngestionNodeApplication.java
+++ src/main/java/org/icgc_argo/workflowingestionnode/WorkflowIngestionNodeApplication.java
@@ -22,7 +22,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// @EnableAutoConfiguration(exclude={ContextFunctionCatalogAutoConfiguration.class})
 public class WorkflowIngestionNodeApplication {
 
   public static void main(String[] args) {
