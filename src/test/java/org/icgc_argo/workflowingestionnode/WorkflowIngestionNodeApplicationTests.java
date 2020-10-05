c3402b7160989a1f13571462e4e23c613ff31cee
jaserud <udjaser@gmail.com>
2020-10-05 10:03:57 -0400

    change parent pom to graph lib

--- src/test/java/org/icgc_argo/workflowingestionnode/WorkflowIngestionNodeApplicationTests.java
+++ src/test/java/org/icgc_argo/workflowingestionnode/WorkflowIngestionNodeApplicationTests.java
@@ -1,6 +1,5 @@
 package org.icgc_argo.workflowingestionnode;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +7,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 class WorkflowIngestionNodeApplicationTests {
 
   @Test
-  @Disabled // Disabled because context load will fail with out rabbit and kafka running.
   void contextLoads() {}
 }
